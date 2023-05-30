26ca533a5530794ff78ed135f6db8021f83e9609
Hongfei Xu <hfxunlp@foxmail.com>
2023-05-30 10:23:25 +0800

    utils.fmt.vocab.ldvocab: add duplicated token check

--- utils/fmt/doc/mono/base.py
+++ utils/fmt/doc/mono/base.py
@@ -1,43 +1,13 @@
 #encoding: utf-8
 
-from utils.fmt.base import list_reader
 from utils.fmt.vocab.base import no_unk_mapper
+from utils.fmt.vocab.token import ldvocab as ldvocab_base
 
 from cnfg.vocab.doc.mono import *
 
-def ldvocab(vfile, minf=False, omit_vsize=False, init_vocab=init_vocab, init_normal_token_id=init_normal_token_id):
+def ldvocab(*args, init_vocab=init_vocab, init_normal_token_id=init_normal_token_id, **kwargs):
 
-	rs = init_vocab.copy()
-	cwd = init_normal_token_id
-	if omit_vsize:
-		vsize = omit_vsize
-	else:
-		vsize = False
-	for data in list_reader(vfile, keep_empty_line=False):
-		freq = int(data[0])
-		if (not minf) or freq > minf:
-			if vsize:
-				ndata = len(data) - 1
-				if vsize >= ndata:
-					for wd in data[1:]:
-						rs[wd] = cwd
-						cwd += 1
-				else:
-					for wd in data[1:vsize + 1]:
-						rs[wd] = cwd
-						cwd += 1
-						ndata = vsize
-					break
-				vsize -= ndata
-				if vsize <= 0:
-					break
-			else:
-				for wd in data[1:]:
-					rs[wd] = cwd
-					cwd += 1
-		else:
-			break
-	return rs, cwd
+	return ldvocab_base(*args, init_vocab=init_vocab, init_normal_token_id=init_normal_token_id, **kwargs)
 
 def map_batch_core(i_d, vocabi, use_unk=use_unk, sos_id=sos_id, eos_id=eos_id, unk_id=unk_id, cls_id=cls_id, **kwargs):
 
--- utils/fmt/vocab/token.py
+++ utils/fmt/vocab/token.py
@@ -8,7 +8,7 @@ from cnfg.vocab.base import init_normal_token_id, init_vocab
 
 sep_load, sep_save = None, " "
 
-def ldvocab(vfile, minf=False, omit_vsize=False, vanilla=False, init_vocab=init_vocab, init_normal_token_id=init_normal_token_id, sep=sep_load, file_reader=file_reader):
+def ldvocab(vfile, minf=False, omit_vsize=False, vanilla=False, init_vocab=init_vocab, init_normal_token_id=init_normal_token_id, sep=sep_load, file_reader=file_reader, print_func=print):
 
 	if vanilla:
 		rs, cwd = {}, 0
@@ -18,30 +18,50 @@ def ldvocab(vfile, minf=False, omit_vsize=False, vanilla=False, init_vocab=init_
 		vsize = max(0, omit_vsize - cwd)
 	else:
 		vsize = False
+	dkeys = set()
 	for data in file_reader(vfile, keep_empty_line=False, sep=sep):
 		freq = int(data[0])
-		if (not minf) or freq > minf:
+		if (not minf) or (freq > minf):
 			if vsize:
 				ndata = len(data) - 1
-				if vsize >= ndata:
+				if vsize > ndata:
 					for wd in data[1:]:
-						rs[wd] = cwd
-						cwd += 1
+						if wd in rs:
+							if wd not in dkeys:
+								dkeys.add(wd)
+							ndata -= 1
+						else:
+							rs[wd] = cwd
+							cwd += 1
+					vsize -= ndata
 				else:
-					for wd in data[1:vsize + 1]:
-						rs[wd] = cwd
-						cwd += 1
-						ndata = vsize
-					break
-				vsize -= ndata
-				if vsize <= 0:
-					break
+					_break = False
+					for wd in data[1:]:
+						if wd in rs:
+							if wd not in dkeys:
+								dkeys.add(wd)
+						else:
+							rs[wd] = cwd
+							cwd += 1
+							vsize -= 1
+							_break = (vsize <= 0)
+							if _break:
+								break
+					if _break:
+						break
 			else:
 				for wd in data[1:]:
-					rs[wd] = cwd
-					cwd += 1
+					if wd in rs:
+						if wd not in dkeys:
+							dkeys.add(wd)
+					else:
+						rs[wd] = cwd
+						cwd += 1
 		else:
 			break
+	if (print_func is not None) and dkeys:
+		print_func("duplicated vocab keys: %s" % str(dkeys))
+
 	return rs, cwd
 
 def save_vocab(vcb_dict, fname, omit_vsize=False, sep=sep_save):
@@ -72,38 +92,63 @@ def save_vocab(vcb_dict, fname, omit_vsize=False, sep=sep_save):
 				if remain <= 0:
 					break
 
-def ldvocab_list(vfile, minf=False, omit_vsize=False, sep=sep_load, file_reader=file_reader):
+def ldvocab_list(vfile, minf=False, omit_vsize=False, sep=sep_load, file_reader=file_reader, print_func=print):
 
 	rs = []
 	if omit_vsize:
 		vsize = omit_vsize
 	else:
 		vsize = False
-	cwd = 0
+	cwd, lwd, dkeys = 0, set(), set()
 	for data in file_reader(vfile, keep_empty_line=False, sep=sep):
 		freq = int(data[0])
-		if (not minf) or freq > minf:
+		if (not minf) or (freq > minf):
 			if vsize:
 				ndata = len(data) - 1
-				if vsize >= ndata:
-					rs.extend(data[1:])
-					cwd += ndata
+				if vsize > ndata:
+					for wd in data[1:]:
+						if wd in lwd:
+							if wd not in dkeys:
+								dkeys.add(wd)
+							ndata -= 1
+						else:
+							rs.append(wd)
+							lwd.add(wd)
+							cwd += 1
+					vsize -= ndata
 				else:
-					rs.extend(data[1:vsize + 1])
-					cwd += vsize
-					break
-				vsize -= ndata
-				if vsize <= 0:
-					break
+					_break = False
+					for wd in data[1:]:
+						if wd in lwd:
+							if wd not in dkeys:
+								dkeys.add(wd)
+						else:
+							rs.append(wd)
+							lwd.add(wd)
+							cwd += 1
+							vsize -= 1
+							_break = (vsize <= 0)
+							if _break:
+								break
+					if _break:
+						break
 			else:
-				rs.extend(data[1:])
-				cwd += len(data) - 1
+				for wd in data[1:]:
+					if wd in lwd:
+						if wd not in dkeys:
+							dkeys.add(wd)
+					else:
+						rs.append(wd)
+						lwd.add(wd)
+						cwd += 1
 		else:
 			break
+	if (print_func is not None) and dkeys:
+		print_func("duplicated vocab keys: %s" % str(dkeys))
 
 	return rs, cwd
 
-def ldvocab_freq(vfile, minf=False, omit_vsize=False, sep=sep_load, file_reader=file_reader):
+def ldvocab_freq(vfile, minf=False, omit_vsize=False, sep=sep_load, file_reader=file_reader, print_func=print):
 
 	rs = {}
 	if omit_vsize:
@@ -111,28 +156,48 @@ def ldvocab_freq(vfile, minf=False, omit_vsize=False, sep=sep_load, file_reader=
 	else:
 		vsize = False
 	cwd = 0
+	dkeys = set()
 	for data in file_reader(vfile, keep_empty_line=False, sep=sep):
 		freq = int(data[0])
-		if (not minf) or freq > minf:
+		if (not minf) or (freq > minf):
 			if vsize:
 				ndata = len(data) - 1
-				if vsize >= ndata:
-					for _ in data[1:]:
-						rs[_] = freq
+				if vsize > ndata:
+					for wd in data[1:]:
+						if wd in rs:
+							if wd not in dkeys:
+								dkeys.add(wd)
+							ndata -= 1
+						else:
+							rs[wd] = freq
 					cwd += ndata
+					vsize -= ndata
 				else:
-					for _ in data[1:vsize + 1]:
-						rs[_] = freq
-					cwd += vsize
-					break
-				vsize -= ndata
-				if vsize <= 0:
-					break
+					_break = False
+					for wd in data[1:]:
+						if wd in rs:
+							if wd not in dkeys:
+								dkeys.add(wd)
+						else:
+							rs[wd] = freq
+							cwd += 1
+							vsize -= 1
+							_break = (vsize <= 0)
+							if _break:
+								break
+					if _break:
+						break
 			else:
-				for _ in data[1:]:
-					rs[_] = freq
-				cwd += len(data) - 1
+				for wd in data[1:]:
+					if wd in rs:
+						if wd not in dkeys:
+							dkeys.add(wd)
+					else:
+						rs[wd] = freq
+						cwd += 1
 		else:
 			break
+	if (print_func is not None) and dkeys:
+		print_func("duplicated vocab keys: %s" % str(dkeys))
 
 	return rs, cwd
