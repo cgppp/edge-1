e4f964cbddd365c288fa6b20211f91a847f55807
Hongfei Xu <hfxunlp@foxmail.com>
2023-06-24 12:39:49 +0800

    update utils.fmt.gec.noise.base

--- utils/fmt/gec/noise/base.py
+++ utils/fmt/gec/noise/base.py
@@ -1,6 +1,7 @@
 #encoding: utf-8
 
-from random import choices, randint, sample, shuffle
+from math import ceil, floor
+from random import choices, gauss, randint, sample, shuffle
 
 from utils.fmt.base import sys_open
 from utils.fmt.parser import parse_none
@@ -32,26 +33,14 @@ def filter_bi_same(samples, uni):
 
 	return _b if _a == uni else _a
 
-class NoiserBase:
-
-	def edit(self, x, **kwargs):
-
-		return x
-
-	def __call__(self, x, sind, k, **kwargs):
-
-		_eind = sind + k
-
-		return "%s%s%s" % (x[:sind], self.edit(x[sind:_eind], **kwargs), x[_eind:],)
-
-class CharReplacer(NoiserBase):
+class CharReplacer:
 
 	def __init__(self, df, sample_func=sample):
 
 		self.rpd = load_replace_data(df)
 		self.sample_func = sample_func
 
-	def edit(self, x, sample_func=None, data=None):
+	def __call__(self, x, sample_func=None, data=None, **kwargs):
 
 		_sample_func, _rpd = parse_none(sample_func, self.sample_func), parse_none(data, self.rpd)
 		rs = []
@@ -60,53 +49,55 @@ class CharReplacer(NoiserBase):
 
 		return "".join(rs)
 
-class VocabReplacer(NoiserBase):
+class VocabReplacer:
 
-	def __init__(self, df, vsize=vocab_size-init_token_id, sample_func=sample):
+	def __init__(self, df, ls=1.0, sample_func=sample):
 
 		self.rpd = ldvocab_list(df)[0]
-		self.sample_func = sample_func
+		self.ls, self.sample_func = ls, sample_func
 
-	def edit(self, x, sample_func=None, data=None):
+	def __call__(self, x, sample_func=None, data=None, **kwargs):
 
 		_sample_func, _rpd = parse_none(sample_func, self.sample_func), parse_none(data, self.rpd)
 		_src_s = set(x)
-		_src_len = len(x)
-		rs = [_ for _ in _sample_func(_rpd, _src_len + len(_src_s)) if _ not in _src_s]
+		_rs_len = len(x)
+		if self.ls != 0.0:
+			_ = gauss(mu=0.0, sigma=self.ls)
+			_ = (floor if _ < 0.0 else ceil)(_)
+			if _ != 0:
+				_rs_len = max(1, _rs_len + _)
+		rs = [_ for _ in _sample_func(_rpd, _rs_len + len(_src_s)) if _ not in _src_s]
 
-		return "".join(rs[:_src_len])
+		return "".join(rs[:_rs_len])
 
-class Shuffler(NoiserBase):
+def shuffler(x, **kwargs):
 
-	def edit(self, x, **kwargs):
+	_ = list(x)
+	shuffle(_)
 
-		_ = list(x)
-		shuffle(_)
+	return "".join(_)
 
-		return "".join(_)
+def repeat(x, **kwargs):
 
-def repeat(x, sind, k, **kwargs):
+	return "%s%s" % (x, x,)
 
-	_eind = sind + k
-	_ = x[sind:_eind]
+def drop(x, **kwargs):
 
-	return "%s%s%s%s" % (x[:sind], _, _, x[_eind:],)
+	return ""
 
-def drop(x, sind, k, **kwargs):
+def select_noise_span(spl, l):
 
-	return "%s%s" % (x[:sind], x[sind + k:],)
-
-def sorted_keep_span(spl, l):
-
-	_l = 0
-	_ = {}
-	for _sind, _span_len in spl:
-		_l += _span_len
-		if (_l > l) and _:
-			break
-		_[_sind] = (_sind, _span_len,)
-	for _sind in sorted(_.keys()):
-		yield _[_sind]
+	_ = [(i, len(_tmp),) for i, _tmp in enumerate(spl)]
+	shuffle(_)
+	_ind, _span_len = _[0]
+	yield _ind
+	_l = l - _span_len
+	if _l > 0:
+		for _ind, _span_len in _[1:]:
+			_l -= _span_len
+			if (_l < 0):
+				break
+			yield _ind
 
 class Noiser:
 
@@ -114,7 +105,6 @@ class Noiser:
 
 		self.edits = []
 		w = []
-		self.inc_ind = self.dec_ind = self.shuf_ind = None
 		if char is not None:
 			if isinstance(char, str):
 				self.edits.append(CharReplacer(char))
@@ -131,15 +121,12 @@ class Noiser:
 			self.edits.append(VocabReplacer(vcb))
 			w.append(w_vcb)
 		if w_shuf > 0.0:
-			self.shuf_ind = len(self.edits)
-			self.edits.append(Shuffler())
+			self.edits.append(shuffler)
 			w.append(w_shuf)
 		if w_repeat > 0.0:
-			self.inc_ind = len(self.edits)
 			self.edits.append(repeat)
 			w.append(w_repeat)
 		if w_drop > 0.0:
-			self.dec_ind = len(self.edits)
 			self.edits.append(drop)
 			w.append(w_drop)
 		self.sample_cw = cumsum(pos_norm(w))
@@ -151,31 +138,22 @@ class Noiser:
 		_r_len = len(x)
 		if _r_len == 1:
 			return x
-		_last_ind = _r_len - 1
-		_min_span_len, _max_span_len, _sample_ind, _sample_cw, _inc_ind, _dec_ind, _shuf_ind = self.min_span_len, self.max_span_len, self.sample_ind, self.sample_cw, self.inc_ind, self.dec_ind, self.shuf_ind
+		_min_span_len, _max_span_len, _sample_ind, _sample_cw = self.min_span_len, self.max_span_len, self.sample_ind, self.sample_cw
 		_corr_len = max(int(_r_len * self.p), 1)
 		_min_span_len, _max_span_len = min(_min_span_len, _corr_len), min(_max_span_len, _corr_len)
 		_sind = 0
 		_spans = []
-		while _r_len > 0:
+		while _sind < _r_len:
 			_span_len = 1 if _max_span_len == 1 else min(randint(_min_span_len, _max_span_len), _r_len)
-			_spans.append((_sind, _span_len,))
-			_sind += _span_len
-			_r_len -= _span_len
-		shuffle(_spans)
-		_shift = 0
-		rs = x
-		for _sind, _span_len in sorted_keep_span(_spans, _corr_len):
-			_ind = choices(_sample_ind, cum_weights=_sample_cw, k=1)[0]
-			_r_sind = _sind + _shift
-			if (_ind == _shuf_ind) and (_span_len == 1):
-				_span_len = 2
-				if _sind == _last_ind:
-					_r_sind -= 1
-			rs = self.edits[_ind](rs, _r_sind, _span_len, **kwargs)
-			if _ind == _inc_ind:
-				_shift += _span_len
-			elif _ind == _dec_ind:
-				_shift -= _span_len
+			_eind = _sind + _span_len
+			_spans.append(x[_sind:_eind])
+			_sind = _eind
+		_sinds = set(select_noise_span(_spans, _corr_len))
+		rs = []
+		for _ind, _ in enumerate(_spans):
+			if _ind in _sinds:
+				rs.append(self.edits[choices(_sample_ind, cum_weights=_sample_cw, k=1)[0]](_, **kwargs))
+			else:
+				rs.append(_)
 
-		return rs
+		return "".join(rs)
