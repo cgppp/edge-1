140320186f9fe73927648bda734327ea91291f68
Hongfei Xu <hfxunlp@foxmail.com>
2023-11-28 09:47:58 +0800

    utils.fmt.gec.noise.cust: load pairs from file

--- utils/fmt/gec/noise/cust.py
+++ utils/fmt/gec/noise/cust.py
@@ -3,6 +3,7 @@
 from random import random
 from re import compile
 
+from utils.fmt.base import sys_open
 from utils.fmt.rext import SubPair, SubPairP
 
 mdate_search = compile("\d+月\d+日").search
@@ -10,8 +11,32 @@ renminzhengfu_sub = compile("[省市县区]人民政府").sub
 jiezhi_search = compile("截至\d+").search
 didei_sub = compile("地|得").sub
 
-yicuo_pairs = (("登录", "登陆",),)
-yicuo_sub = SubPair(yicuo_pairs, allow_re=False).handle
+tihuan_file = "custdata/随机替换.txt"
+huhuan_file = "custdata/随机互换.txt"
+shanchu_file = "custdata/随机删除.txt"
+
+def reader(fname):
+
+	with sys_open(fname, "rb") as f:
+		for line in f:
+			tmp = line.strip()
+			if tmp:
+				tmp = tmp.decode("utf-8")
+				if not tmp.startswith("#"):
+					tmp = tuple(_ for _ in tmp.split() if _)
+					yield tmp
+
+def load_pairs(fname):
+
+	rs = set()
+	for _ in reader(fname):
+		if len(_) == 2:
+			if _ not in rs:
+				rs.add(_)
+	rs = list(rs)
+	rs.sort()
+
+	return tuple(rs)
 
 def make_pairs(lin):
 
@@ -21,7 +46,39 @@ def make_pairs(lin):
 			if i != j:
 				yield s, t
 
-yicuo_p_pairs = tuple(make_pairs(["中央", "党中央", "中共中央"]))
+def load_p_pairs(fname):
+
+	prs = set()
+	for _ in reader(fname):
+		_ = tuple(set(_))
+		if len(_) > 1:
+			if _ not in prs:
+				prs.add(_)
+	rs = set()
+	for _ in prs:
+		if len(_) == 2:
+			if _ not in rs:
+				rs.add(_)
+		else:
+			rs |= set(make_pairs(_))
+
+	return tuple(sorted(rs))
+
+def load_del(fname):
+
+	rs = set()
+	for _ in reader(fname):
+		if len(_) == 1:
+			_ = _[0]
+			if _ not in rs:
+				rs.add(_)
+
+	return tuple((_, "",) for _ in sorted(rs))
+
+yicuo_pairs = load_pairs(tihuan_file)
+yicuo_sub = SubPair(yicuo_pairs, allow_re=False).handle
+
+yicuo_p_pairs = tuple(sorted(load_p_pairs(huhuan_file) + load_del(shanchu_file)))
 yicuo_p_sub = SubPairP(yicuo_p_pairs, allow_re=False, p=0.1).handle
 
 def mdate_err(x, *args, sfunc=mdate_search, **kwargs):
