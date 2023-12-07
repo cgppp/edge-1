101af9778c8c1c6ec95072fedcab13f914e5ac0e
Hongfei Xu <hfxunlp@foxmail.com>
2023-12-07 15:55:35 +0800

    utils.propos: support custom normer

--- utils/propos.py
+++ utils/propos.py
@@ -2,9 +2,12 @@
 
 import torch
 
+from modules.normer import MRNormer, MinNormer
 from utils.torch.comp import mask_tensor_type
 
-def pos2p(num_pos, length, scale=1.0, sid=0, device=None, dtype=torch.float):
+normer = torch.nn.Softmax(-1)
+
+def pos2p(num_pos, length, scale=1.0, sid=0, device=None, dtype=torch.float, normer=normer):
 
 	_inds = torch.arange(length, device=device, dtype=dtype).unsqueeze(0)
 	_ = torch.arange(sid, length, device=device, dtype=dtype)
@@ -13,4 +16,4 @@ def pos2p(num_pos, length, scale=1.0, sid=0, device=None, dtype=torch.float):
 	_d = ((_inds / _.unsqueeze(-1)).unsqueeze(-1) - torch.arange(num_pos, device=device, dtype=dtype).div_(num_pos - 1)).abs_()
 	_m = torch.ones(length - sid, length, device=device, dtype=mask_tensor_type).triu_(sid + 1)
 
-	return (_d.neg_() if scale == 1.0 else _d.mul_(-scale)).softmax(-1).masked_fill_(_m.unsqueeze(-1), 0.0)
+	return normer(_d.neg_() if scale == 1.0 else _d.mul_(-scale)).masked_fill_(_m.unsqueeze(-1), 0.0)
