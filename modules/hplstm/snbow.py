b96ab2e031e7b315cfc47b8dda105443a6c61c1f
Hongfei Xu <hfxunlp@foxmail.com>
2024-07-16 09:18:58 +0800

    modules.hplstm.snbow: dbg dec

--- modules/hplstm/snbow.py
+++ modules/hplstm/snbow.py
@@ -39,7 +39,7 @@ class MHPLSTMCore(MHPLSTMCoreBase):
 			fgate = fgate.masked_fill(head_mask, 1.0)
 			igh.masked_fill_(head_mask, 0.0)
 
-		cell = LGateFunc(fgate, igh, self.init_cx, True) if states is None else igh.addcmul_(fgate, self.init_cx if _init_state else states[-1])
+		cell = LGateFunc(fgate, igh, self.init_cx, True) if states is None else igh.addcmul_(fgate, self.init_cx if states == "init" else states)
 		out = self.trans_og(torch.cat((heads_input, cell), dim=-1)).sigmoid() * cell
 
 		return out if states is None else (out, cell,)
