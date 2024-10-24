e683a3bb822d598609f275a78bf20c18c4777f9a
Hongfei Xu <hfxunlp@foxmail.com>
2024-10-24 15:29:07 +0800

    init transformer.selfkd.hplstmdecoder

---
+++ transformer/SelfKD/HPLSTMDecoder.py
@@ -0,0 +1,48 @@
+#encoding: utf-8
+
+import torch
+
+from modules.kd.base import GradientAdapterFunc
+from transformer.HPLSTM.FNDecoder import Decoder as DecoderBase
+from utils.kd.self.feat import get_kd_loss
+
+from cnfg.ihyp import *
+from cnfg.vocab.base import pad_id
+
+class Decoder(DecoderBase):
+
+	def __init__(self, isize, nwd, num_layer, fhsize=None, dropout=0.0, attn_drop=0.0, act_drop=None, emb_w=None, num_head=8, xseql=cache_len_default, ahsize=None, norm_output=True, bindemb=True, forbidden_index=None, kd_layers=None, min_sim=0.0, share_layer=False, disable_pemb=disable_std_pemb_decoder, **kwargs):
+
+		super(Decoder, self).__init__(isize, nwd, num_layer, fhsize=fhsize, dropout=dropout, attn_drop=attn_drop, act_drop=act_drop, emb_w=emb_w, num_head=num_head, xseql=xseql, ahsize=ahsize, norm_output=norm_output, bindemb=bindemb, forbidden_index=forbidden_index, share_layer=share_layer, disable_pemb=True, **kwargs)
+
+		self.kd_layers = set() if kd_layers is None else (kd_layers if isinstance(kd_layers, set) else set(kd_layers))
+		self.min_sim = min_sim
+
+	def forward(self, inpute, inputo, src_pad_mask=None, gold=None, gold_pad_mask=None, **kwargs):
+
+		out = self.wemb(inputo)
+
+		if self.drop is not None:
+			out = self.drop(out)
+
+		_use_kd = self.training and (gold is not None)
+		if _use_kd:
+			kd_o = []
+		for prev_layer_ind, net in enumerate(self.nets):
+			if _use_kd and (prev_layer_ind in self.kd_layers):
+				out, _ = GradientAdapterFunc(out, self.min_sim)
+				kd_o.append(_)
+			out = net(inpute, out, src_pad_mask=src_pad_mask)
+
+		if self.out_normer is not None:
+			out = self.out_normer(out)
+
+		_last_layer_out = out
+		out = self.lsm(self.classifier(out))
+
+		if _use_kd:
+			if (prev_layer_ind + 1) in self.kd_layers:
+				kd_o.append(_last_layer_out)
+			return out, get_kd_loss(torch.stack(kd_o, dim=0), mask=gold.eq(pad_id) if gold_pad_mask is None else gold_pad_mask) if len(kd_o) > 1 else out.new_zeros(1)
+		else:
+			return out
