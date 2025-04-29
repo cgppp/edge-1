17eabdcb5807a9d1be2443e550df8fbf02636b95
Hongfei Xu <hfxunlp@foxmail.com>
2025-04-29 21:46:24 +0800

    tools.plm: add qwen.v3 (without SFT support for think)

---
+++ tools/plm/token/qwen/v3.py
@@ -0,0 +1,17 @@
+#encoding: utf-8
+
+import sys
+from transformers import Qwen2TokenizerFast as Tokenizer
+
+from utils.fmt.base import loop_file_so
+
+from cnfg.vocab.plm.qwen.v3 import templated
+
+def handle(fsrc, vcb, frs, template="instruct", system="You are a helpful assistant.", templated=templated):
+
+	_tfunc = templated[template]
+	_ = lambda lin, processor: " ".join(processor(_tfunc(system, lin)))
+	return loop_file_so(fsrc, frs, process_func=_, processor=Tokenizer.from_pretrained(vcb).tokenize)
+
+if __name__ == "__main__":
+	handle(*sys.argv[1:])
